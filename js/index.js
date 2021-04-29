24264ec55a65fcd69227829e230bf33180ce5926
Freebiespic <pozanmatang@gmail.com>
2021-04-29 08:10:54 +0700

    update 1 post
    Post: Cara membuat theme wordpress

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 $(function() {
     var header = $("#headerian");
     $(window).scroll(function() {    
-        var scroll = $(window).scrollTop();
+        const scroll = $(window).scrollTop();
     
         if (scroll >= 500) {
             header.removeClass('py2').addClass("py1");
